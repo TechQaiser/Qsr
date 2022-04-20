ec90a14e680a1db3bc81e85b39f1f4caa0dad0e7
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-04-21 00:40:03 +0500

    Update Qaiser.py

--- Qaiser.py
+++ Qaiser.py
@@ -1,2 +1,20 @@
+import os, sys, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+if not os.path.isfile('.agents.txt'):
+
+    os.system('curl -L https://raw.githubusercontent.com/TechQaiser/ap/main/.agents.txt > .agents.txt')
+
+    os.system('clear')
+
 from Qsr import qsbuy
+
 qsbuy()
+
