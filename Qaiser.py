007df605c281dce111dae752346f2a5261cf06f7
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-04-27 23:23:20 +0500

    Add files via upload

--- Qaiser.py
+++ Qaiser.py
@@ -1,16 +1,6 @@
-import os, sys, platform
+import os
+import Qsr
 os.system('git pull')
 os.system('termux-setup-storage')
-
-
-
-
-
-
-
-os.system('xdg-open https://facebook.com/groups/2734614926615884/')
-
 from Qsr import qsbuy
-
 qsbuy()
-
