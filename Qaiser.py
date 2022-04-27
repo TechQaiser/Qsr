1e9e79edde573ab8cfe62c8a85fad49b9acd0c53
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-04-27 23:04:51 +0500

    Update Qaiser.py

--- Qaiser.py
+++ Qaiser.py
@@ -1,13 +1,13 @@
 import os, sys, platform
 os.system('git pull')
 os.system('termux-setup-storage')
-try:
 
-    import requests
 
-except:
 
-    os.system('pip install requests')
+
+
+
+
 os.system('xdg-open https://facebook.com/groups/2734614926615884/')
 
 from Qsr import qsbuy
