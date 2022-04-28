9a567e63fe4b5c2ba5bec9eeea195118c5f425f1
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-04-29 00:24:32 +0500

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-#run file qsr
-import platform
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Qsr32").qsbuy()
-elif 'aarch' in arc:
-	__import__("Qsr").qsbuy()
-else:
-	exit(f' Bro Your Device Is Unknow {arc} \n Contact Admin For Solution +923079321417')
