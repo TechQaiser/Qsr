09dded797846c85a7fe0aad12a47cc3fe812d54a
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-04-29 00:24:56 +0500

    Add files via upload

--- Qaiser.py
+++ Qaiser.py
@@ -1,6 +1,9 @@
-
-
-
-
-from Qsr import qsbuy
-qsbuy()
+#run file qsr
+import platform
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("Qsr32").qsbuy()
+elif 'aarch' in arc:
+	__import__("Qsr").qsbuy()
+else:
+	exit(f' Bro Your Device Is Unknow {arc} \n Contact Admin For Solution +923079321417')
