3451141c76718e67ca50baf54bbab908fabb4b09
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-08-18 17:22:44 +0500

    Update Qaiser.py

--- Qaiser.py
+++ Qaiser.py
@@ -2,8 +2,8 @@
 import platform
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Qsr32").qsbuy()
+	__import__("qssr").qsbuy()
 elif 'aarch' in arc:
-	__import__("Qsr").qsbuy()
+	__import__("qssr").qsbuy()
 else:
 	exit(f' Bro Your Device Is Unknow {arc} \n Contact Admin For Solution +923079321417')
