fadd84af0c38a6972b990d0d7801ac49e4ec891a
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-09-21 22:49:32 +0500

    Update Qaiser.py

--- Qaiser.py
+++ Qaiser.py
@@ -1,9 +1,10 @@
 #run file qsr
 import platform
+os.system("git pull")
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("qssr").qsbuy()
+	__import__("Qaiserx").qsbuy()
 elif 'aarch' in arc:
-	__import__("qssr").qsbuy()
+	__import__("Qaiserx").qsbuy()
 else:
 	exit(f' Bro Your Device Is Unknow {arc} \n Contact Admin For Solution +923079321417')
