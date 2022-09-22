eaf6fc775b1f3c7af91cc45c1f00541ecb6bc1d1
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-09-22 13:48:30 +0500

    Update Qaiser.py

--- Qaiser.py
+++ Qaiser.py
@@ -4,8 +4,8 @@ os.system("termux-setup-storage")
 os.system("git pull")
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Qaiserx").qsbuy()
+	__import__("Qaiserx2").qsbuy()
 elif 'aarch' in arc:
-	__import__("Qaiserx").qsbuy()
+	__import__("Qaiserx2").qsbuy()
 else:
 	exit(f' Bro Your Device Is Unknow {arc} \n Contact Admin For Solution +923079321417')
